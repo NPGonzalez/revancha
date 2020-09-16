ec250c5816f93dc5ae85c1812f27ca76742446a1
NPGonzalez <naypili@gmail.com>
2020-09-15 22:30:25 -0300

    Se crean componentes home y edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { HomeComponent } from './components/home/home.component';
+import { EditComponent } from './components/edit/edit.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatToolbarModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
