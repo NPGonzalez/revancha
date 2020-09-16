3c16382c6835d082f287d7a0a17223589699e13b
NPGonzalez <naypili@gmail.com>
2020-09-16 02:46:57 -0300

    Se trabaja en el home y se despliega data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { HomeComponent } from './components/home/home.component';
 import { EditComponent } from './components/edit/edit.component';
 import { MaterialModule } from './material.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './srevices/data.service';
+
 
 
 @NgModule({
@@ -25,10 +28,13 @@ import { MaterialModule } from './material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatSliderModule,
     MaterialModule,
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 
 
@@ -14,11 +20,21 @@ import { MatInputModule } from '@angular/material/input';
     MatToolbarModule,
     MatInputModule,
     MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   exports: [
     MatToolbarModule,
     MatInputModule,
     MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ]
 })
 export class MaterialModule { }
